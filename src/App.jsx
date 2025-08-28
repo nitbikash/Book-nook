83d0c281ed1351db429f73c7447f2529563b654a
Bikash Tandi <124274128+nitbikash@users.noreply.github.com>
2025-08-29 01:10:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Outlet} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <>
+        <NavBar/>
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+            <Outlet/>
+        </main>
+        <Footer/>
+    </>
+  )
+}
+
+export default App
